72a7b67f658ccf7abe1e6fd727ed8ce936c2293d
rpardon <romain.pardon@almerys.com>
2018-10-03 14:07:34 +0200

    Add Rxdart to merge streams.

--- lib/src/screens/login_screen.dart
+++ lib/src/screens/login_screen.dart
@@ -1,27 +1,26 @@
 import 'package:flutter/material.dart';
+import '../blocs/provider.dart';
 import '../blocs/bloc.dart';
-
-final bloc = new Bloc();
 
 class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    
+    final bloc = Provider.of(context);
 
     return Container(
       margin: EdgeInsets.all(20.0),
       child: Column(
         children: [
-          emailField(),
-          passwordField(),
+          emailField(bloc),
+          passwordField(bloc),
           Container(margin: EdgeInsets.only(top:25.0)),
-          submitButton()
+          submitButton(bloc)
         ],
       ),
     );
   }
 
-  Widget emailField() {
+  Widget emailField(Bloc bloc) {
     return StreamBuilder(
       stream: bloc.email,
       builder: (context, snapshot) {
@@ -40,7 +39,7 @@ class LoginScreen extends StatelessWidget {
      
   }
 
-  Widget passwordField() {
+  Widget passwordField(Bloc bloc) {
     return StreamBuilder(
       stream: bloc.password,
       builder: (context, snapshot) {
@@ -58,12 +57,18 @@ class LoginScreen extends StatelessWidget {
     
   }
 
-  Widget submitButton( ){
-    return RaisedButton(
-      child: Text('Login'),
-      color: Colors.blue,
-      onPressed: () {}
+  Widget submitButton(Bloc bloc){
+    return StreamBuilder(
+      stream: bloc.submitValid,
+      builder: (context, snapshot) {
+        return RaisedButton(
+          child: Text('Login'),
+          color: Colors.blue,
+          onPressed: () {}
+        );
+      },
     );
+    
   }
 
 }
